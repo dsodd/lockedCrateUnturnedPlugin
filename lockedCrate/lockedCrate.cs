c90cf5a6d55fd9c3f0be12b184b59af4363964dc
dsodd <pajicvanja70@gmail.com>
2025-04-20 00:20:40 +0200

    Add project files.

---
+++ lockedCrate/lockedCrate.cs
@@ -0,0 +1,76 @@
+﻿using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace lockedCrate
+{
+    public class LockedCrate : RocketPlugin<LockedCrateConfiguration>
+    {
+        private BarricadeDrop spawnedCrate;
+
+        protected override void Load()
+        {
+            U.Events.OnPlayerConnected += OnPlayerConnected;
+            BarricadeManager.onOpenStorageRequested += OnOpenStorageRequested;
+
+            SpawnLockedCrate();
+            Logger.Log("LockedCrate loaded.");
+        }
+
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= OnPlayerConnected;
+            BarricadeManager.onOpenStorageRequested -= OnOpenStorageRequested;
+        }
+
+        private void OnPlayerConnected(UnturnedPlayer player)
+        {
+            if (spawnedCrate == null)
+            {
+                SpawnLockedCrate();
+            }
+        }
+
+        private void SpawnLockedCrate()
+        {
+            if (Configuration.Instance.SpawnLocations == null || Configuration.Instance.SpawnLocations.Count == 0)
+            {
+                Logger.LogError("No spawn locations defined in configuration.");
+                return;
+            }
+
+            var random = new System.Random();
+            var location = Configuration.Instance.SpawnLocations[random.Next(Configuration.Instance.SpawnLocations.Count)].ToVector3();
+
+            var asset = Assets.find(EAssetType.ITEM, Configuration.Instance.CrateId);
+            var crateAsset = asset as ItemBarricadeAsset;
+            if (crateAsset == null)
+            {
+                Logger.LogError($"Crate ID {Configuration.Instance.CrateId} not found or is not a barricade.");
+                return;
+            }
+
+            var crate = new Barricade(crateAsset);
+            Transform transform = BarricadeManager.dropNonPlantedBarricade(crate, location, Quaternion.identity, 0, 0);
+            spawnedCrate = BarricadeManager.FindBarricadeByRootTransform(transform);
+        }
+
+        private void OnOpenStorageRequested(CSteamID steamID, InteractableStorage storage, ref bool shouldAllow)
+        {
+            if (spawnedCrate == null || storage == null)
+                return;
+
+            if (storage.transform == spawnedCrate.model || storage.transform.IsChildOf(spawnedCrate.model))
+            {
+                shouldAllow = false; // block opening the crate
+            }
+        }
+    }
+}
---
+++ lockedCrate/lockedCrateConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace lockedCrate
+{
+    public class LockedCrateConfiguration : IRocketPluginConfiguration
+    {
+        [XmlElement("CrateId")]
+        public ushort CrateId { get; set; } = 366;
+
+        [XmlElement("SpawnTable")]
+        public ushort SpawnTable { get; set; } = 1;
+
+        [XmlElement("UnlockTimer")]
+        public uint UnlockTimer { get; set; } = 15;
+
+        [XmlElement("RespawnTimer")]
+        public uint RespawnTimer { get; set; } = 30;
+
+        [XmlArray("SpawnLocations")]
+        [XmlArrayItem("Location")]
+        public List<Vector3Serializable> SpawnLocations { get; set; }
+
+        public void LoadDefaults()
+        {
+            SpawnLocations = new List<Vector3Serializable>
+            {
+                new Vector3Serializable(1, 1, 1),
+                new Vector3Serializable(2, 2, 2)
+            };
+        }
+    }
+
+    public class Vector3Serializable
+    {
+        [XmlElement("x")]
+        public float X { get; set; }
+
+        [XmlElement("y")]
+        public float Y { get; set; }
+
+        [XmlElement("z")]
+        public float Z { get; set; }
+
+        public Vector3Serializable() { }
+
+        public Vector3Serializable(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3 ToVector3() => new Vector3(X, Y, Z);
+
+        public static Vector3Serializable FromVector3(Vector3 vector) =>
+            new Vector3Serializable(vector.x, vector.y, vector.z);
+    }
+}
